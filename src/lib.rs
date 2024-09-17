5b1cda12786c22f5bf629241e23a0f44f7aaa92c
Avi Dessauer <avi.the.coder@gmail.com>
2024-09-16 20:32:00 -0700

    Re-export `casper_types` crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub(crate) mod crypto;
 pub mod hash;
 pub mod kernel;
 pub mod merkle_proof;
+/// Re-export of `casper_types` crate to allow library consumers to match the version easily.
+pub use casper_types;
