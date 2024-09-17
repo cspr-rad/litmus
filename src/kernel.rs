301f3f4d8a65ee34bf45b819f8431686c789e753
Avi Dessauer <avi.the.coder@gmail.com>
2024-09-16 20:08:12 -0700

    Delete LightClientKernel & Use casper_types::JsonBlockWithSignatures

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,11 +1,7 @@
 use alloc::collections::BTreeMap;
-use casper_types::{EraId, PublicKey, U512};
-use core::option::Option;
+use casper_types::{EraId, JsonBlockWithSignatures, PublicKey, U512};
 
-use crate::{
-    block::BlockHeaderWithSignatures,
-    crypto::{verify, SignatureVerificationError},
-};
+use crate::crypto::{verify, SignatureVerificationError};
 
 use super::block_header::BlockHash;
 
@@ -54,24 +50,19 @@ impl EraInfo {
     #[allow(clippy::result_large_err)]
     pub fn validate(
         &self,
-        block_header_with_signatures: &BlockHeaderWithSignatures,
+        JsonBlockWithSignatures { block, proofs }: JsonBlockWithSignatures,
     ) -> Result<(), BlockSignaturesValidationError> {
         let mut block_signature_weight = U512::from(0);
-        let block_header = block_header_with_signatures.block_header();
-        if block_header.era_id() != self.era_id {
+        if block.era_id() != self.era_id {
             return Err(BlockSignaturesValidationError::WrongEraId {
                 trusted_era_id: self.era_id,
-                block_header_era_id: block_header_with_signatures.block_header().era_id(),
+                block_header_era_id: block.era_id(),
             });
         }
         // See: https://github.com/casper-network/casper-node/blob/8ca9001dabba0dae95f92ad8c54eddd163200b5d/node/src/types/block.rs#L2465-L2474
-        let mut signature_data = block_header.block_hash().as_ref().to_vec();
-        signature_data.extend_from_slice(&block_header.era_id().to_le_bytes());
-        for (public_key, signature) in block_header_with_signatures
-            .block_signatures()
-            .proofs()
-            .iter()
-        {
+        let mut signature_data = block.hash().inner().into_vec();
+        signature_data.extend_from_slice(&block.era_id().to_le_bytes());
+        for (public_key, signature) in proofs.iter() {
             if let Some(validator_weight) = self.validator_weights.get(public_key) {
                 block_signature_weight += *validator_weight;
             } else {
@@ -105,137 +96,5 @@ impl ParentHashAndCurrentHeight {
 
     pub fn current_height(&self) -> u64 {
         self.current_height
-    }
-}
-
-pub struct LightClientKernel {
-    latest_block_hash: BlockHash,
-    parent_hash_and_current_height: Option<ParentHashAndCurrentHeight>,
-    era_info: Option<EraInfo>,
-}
-
-#[derive(Debug)]
-pub enum LightClientUpdateError {
-    InvalidBlockHashWhenInitializing,
-    InvalidBlockHashWhenWalkingBack,
-    BlockInPastWhenProgressing {
-        bad_block_height: u64,
-        current_height: u64,
-    },
-    InvalidSignatures(BlockSignaturesValidationError),
-}
-
-impl From<BlockSignaturesValidationError> for LightClientUpdateError {
-    fn from(block_signature_validation_error: BlockSignaturesValidationError) -> Self {
-        LightClientUpdateError::InvalidSignatures(block_signature_validation_error)
-    }
-}
-
-impl LightClientKernel {
-    pub fn new(latest_block_hash: BlockHash) -> Self {
-        LightClientKernel {
-            latest_block_hash,
-            parent_hash_and_current_height: None,
-            era_info: None,
-        }
-    }
-
-    #[allow(clippy::result_large_err)]
-    pub fn update(
-        &mut self,
-        block_header_with_signatures: &BlockHeaderWithSignatures,
-    ) -> Result<(), LightClientUpdateError> {
-        let block_header = block_header_with_signatures.block_header();
-        match (&self.parent_hash_and_current_height, &self.era_info) {
-            // parent_hash_and_current_height are not set, check the latest block has the correct trusted hash and set them
-            (None, _) => {
-                if self.latest_block_hash == block_header.block_hash() {
-                    let parent_hash = block_header.parent_hash().clone();
-                    let current_height = block_header.height();
-                    self.parent_hash_and_current_height = Some(ParentHashAndCurrentHeight {
-                        parent_hash,
-                        current_height,
-                    });
-                    if let Some(era_end) = block_header.era_end() {
-                        self.era_info = Some(EraInfo::new(
-                            block_header.era_id().successor(),
-                            era_end.next_era_validator_weights().clone(),
-                        ));
-                    }
-                    Ok(())
-                } else {
-                    Err(LightClientUpdateError::InvalidBlockHashWhenInitializing)
-                }
-            }
-            // If the parent_hash_and_current_height are set, but there's no `EraInfo`, then the update effectively walks the light-client back in history by a block.
-            // If the block_header_with_metadata has an era_end, use that to set `EraInfo`.
-            (
-                Some(ParentHashAndCurrentHeight {
-                    parent_hash,
-                    current_height: _,
-                }),
-                None,
-            ) => {
-                if parent_hash == &block_header.block_hash() {
-                    self.latest_block_hash = parent_hash.clone();
-                    let parent_hash = block_header.parent_hash().clone();
-                    let current_height = block_header.height();
-                    self.parent_hash_and_current_height = Some(ParentHashAndCurrentHeight {
-                        parent_hash,
-                        current_height,
-                    });
-                    if let Some(era_end) = block_header.era_end() {
-                        self.era_info = Some(EraInfo::new(
-                            block_header.era_id().successor(),
-                            era_end.next_era_validator_weights().clone(),
-                        ));
-                    }
-                    Ok(())
-                } else {
-                    Err(LightClientUpdateError::InvalidBlockHashWhenWalkingBack)
-                }
-            }
-            // If the era_info is set, then the light client progresses by checking if the block is in the current era and has proper finality signatures. If it is and it has a height higher than the kernel, progress the light client.
-            (
-                Some(ParentHashAndCurrentHeight {
-                    parent_hash: _,
-                    current_height,
-                }),
-                Some(era_info),
-            ) => {
-                if block_header.height() <= *current_height {
-                    Err(LightClientUpdateError::BlockInPastWhenProgressing {
-                        bad_block_height: block_header.height(),
-                        current_height: *current_height,
-                    })
-                } else {
-                    era_info.validate(block_header_with_signatures)?;
-                    self.latest_block_hash = block_header.block_hash();
-                    self.parent_hash_and_current_height = Some(ParentHashAndCurrentHeight {
-                        parent_hash: block_header.parent_hash().clone(),
-                        current_height: block_header.height(),
-                    });
-                    if let Some(era_end) = block_header.era_end() {
-                        self.era_info = Some(EraInfo::new(
-                            block_header.era_id().successor(),
-                            era_end.next_era_validator_weights().clone(),
-                        ));
-                    }
-                    Ok(())
-                }
-            }
-        }
-    }
-
-    pub fn latest_block_hash(&self) -> &BlockHash {
-        &self.latest_block_hash
-    }
-
-    pub fn parent_hash_and_current_height(&self) -> Option<&ParentHashAndCurrentHeight> {
-        self.parent_hash_and_current_height.as_ref()
-    }
-
-    pub fn era_info(&self) -> Option<&EraInfo> {
-        self.era_info.as_ref()
     }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,35 +1,8 @@
-use std::collections::BTreeMap;
-
 use casper_litmus::{
-    block::Block,
     block_header::BlockHeaderV1,
     json_compatibility::{JsonBlock, JsonBlockHeader},
-    kernel::LightClientKernel,
     merkle_proof::{process_query_proofs, TrieMerkleProof},
 };
-use casper_types::{
-    bytesrepr::{deserialize_from_slice, ToBytes},
-    EraId,
-};
-
-use once_cell::sync::Lazy;
-
-static BLOCKS_MAP: Lazy<BTreeMap<u64, JsonBlock>> = Lazy::new(|| {
-    let mut blocks = BTreeMap::new();
-    let cwd = std::env::current_dir().unwrap();
-    let this_files_directory = std::path::Path::new(&(file!())).parent().unwrap();
-    let blocks_path = cwd.join(this_files_directory).join("assets/blocks");
-    for entry in std::fs::read_dir(blocks_path).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_file() {
-            let data = std::fs::read_to_string(path).unwrap();
-            let json_block: JsonBlock = serde_json::from_str(&data).unwrap();
-            blocks.insert(json_block.header().height(), json_block);
-        }
-    }
-    blocks
-});
 
 #[test]
 fn json_block_header_round_trip() {
@@ -38,27 +11,6 @@ fn json_block_header_round_trip() {
     let converted_block_header = BlockHeaderV1::from(json_block.header().clone());
     let reconstituted_json_block_header = JsonBlockHeader::from(converted_block_header.clone());
     assert_eq!(json_block.header(), &reconstituted_json_block_header);
-}
-
-#[test]
-fn update_kernel_one() {
-    let mut kernel = LightClientKernel::new(BLOCKS_MAP.get(&0).unwrap().hash().clone());
-    let json_block: JsonBlock =
-        serde_json::from_str(include_str!("assets/blocks/block-0.json")).unwrap();
-    let block = Block::try_from(json_block.clone()).unwrap();
-    let result = kernel.update(block.block_header_with_signatures());
-    assert!(result.is_ok());
-}
-
-#[test]
-fn update_kernel_history() {
-    let mut kernel = LightClientKernel::new(BLOCKS_MAP.get(&0).unwrap().hash().clone());
-    for height in 0..BLOCKS_MAP.len() {
-        let json_block = BLOCKS_MAP.get(&(height as u64)).unwrap();
-        let block = Block::try_from(json_block.clone()).unwrap();
-        kernel.update(block.block_header_with_signatures()).unwrap();
-        assert_eq!(kernel.latest_block_hash(), json_block.hash());
-    }
 }
 
 #[test]
